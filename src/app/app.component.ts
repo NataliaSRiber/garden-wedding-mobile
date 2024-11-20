fbe78a0f3e0dabbc098ea82933a5d5bb6a8ef8b3
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-11-20 15:29:56 -0300

    feat: atualiza as páginas a serem exibidas no menu mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,13 @@ import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutlin
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/inbox', icon: 'mail' },
+    { title: 'Sobre nós', url: '/folder/outbox', icon: 'paper-plane' },
+    { title: 'Serviços', url: '/folder/favorites', icon: 'heart' },
+    { title: 'Fotos', url: '/folder/archived', icon: 'archive' },
+    { title: 'Agendamento', url: '/folder/trash', icon: 'trash' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = [];
   constructor() {
     addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
   }
