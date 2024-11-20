5a9203dbe81b91adf1943a4b21e1ac1381a837d1
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2024-11-20 15:51:02 -0300

    refactor: recomeça o projeto

--- src/app/pages/loader/loader.page.ts
+++ src/app/pages/loader/loader.page.ts
@@ -1,14 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonSpinner } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.page.html',
   styleUrls: ['./loader.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSpinner]
 })
 export class LoaderPage implements OnInit {
 
